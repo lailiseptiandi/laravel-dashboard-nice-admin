f4dc73cbe2c7d7f1afb63ec5855662135186277e
tpid-laili <laili@technopartner.co.id>
2022-11-19 18:48:21 +0700

    integration dashboard nice admin

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Dashboard - NiceAdmin Bootstrap Template</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    @yield('title')
+    @stack('before-style')
+    @include('includes.style')
+    @stack('after-style')
+</head>
+
+<body>
+    @include('includes.navbar')
+    @include('includes.sidebar')
+
+    @yield('content')
+    @stack('before-script')
+    @include('includes.script')
+    @stack('after-script')
+</body>
+
+</html>
